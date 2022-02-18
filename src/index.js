77b62a99dfb975140b9aced927e546fa5a0c1d1b
LidaChk <lvglazko@gmail.com>
2022-02-18 22:20:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-    stek = [];
+    let stek = [];
     let ret = true;
-    str.split('').forEach(el => {
-        bracketsConfig.forEach(cfg => {
-            if (el === cfg[0] && el === cfg[1]) {
-                if(stek[0] === el){ 
-                  stek.shift();
-                }
-                else {
-                  stek.unshift(el);
-                }
-              } else {
-                if (el === cfg[0]) {
-                  stek.unshift(el);
-                } else if (el === cfg[1]) {
-                  if (stek[0] != cfg[0] || stek.shift() === undefined) {
-                    ret = false;
-                  }
-                } else ret = false;
-              }
-            });
-        });
-      return stek.length === 0 && ret;
+    let strArr = str.split('');
+
+    for (let j =0; j< strArr.length; j++){
+
+      let el = strArr[j];
+
+      for ( let i = 0; i< bracketsConfig.length; i++)
+      {
+        let cfg = bracketsConfig[i];
+
+        if (el === cfg[0] && el === cfg[1]) {
+          if(stek[0] === el){ 
+            stek.shift();
+          }
+          else {
+            stek.unshift(el);
+          }
+        } else {
+          if (el === cfg[0]) {
+            stek.unshift(el);
+          } else if (el === cfg[1]) {
+            if (stek[0] != cfg[0] || stek.shift() === undefined) {
+              return false;
+            }
+          } 
+        }
+      }
+    }
+
+      return stek.length === 0;
     }
