a0611d9a3dc3cf0ab6e4565958c5424968baeae3
LidaChk <lvglazko@gmail.com>
2022-02-18 16:25:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    stek = [];
+    let ret = true;
+    str.split('').forEach(el => {
+        bracketsConfig.forEach(cfg => {
+            if (el === cfg[0] && el === cfg[1]) {
+                if(stek[0] === el){ 
+                  stek.shift();
+                }
+                else {
+                  stek.unshift(el);
+                }
+              } else {
+                if (el === cfg[0]) {
+                  stek.unshift(el);
+                } else if (el === cfg[1]) {
+                  if (stek[0] != cfg[0] || stek.shift() === undefined) {
+                    ret = false;
+                  }
+                } else ret = false;
+              }
+            });
+        });
+      return stek.length === 0 && ret;
+    }
